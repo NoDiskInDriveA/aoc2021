71cda23aa10a5760cb3a51b6a9b8c3061f1ef4fe
Patrick Durold <patrick.durold@inviqa.com>
2021-12-16 14:15:43 +0100

    Day 13 cleanup

--- aoc/day1/tasks.py
+++ aoc/day1/tasks.py
@@ -1,17 +1,22 @@
 from functools import reduce
 from collections import namedtuple
-WINDOW_SIZE = 3
+
 Agg = namedtuple('Agg', ['old', 'incs'], defaults=[None, 0])
 
 
-def main():
+def sum_window(window_size):
     with open('input.txt', 'r') as fp:
         numbers = [int(line.strip()) for line in fp]
         print(reduce(
             lambda agg, val: Agg(old=(new := sum(val)), incs=agg.incs + int(agg.old is not None and new > agg.old)),
-            (numbers[start:start + WINDOW_SIZE] for start in range(0, len(numbers) - WINDOW_SIZE + 1)),
+            (numbers[start:start + window_size] for start in range(0, len(numbers) - window_size + 1)),
             Agg()
         ).incs)
+
+
+def main():
+    sum_window(1)
+    sum_window(3)
 
 
 if __name__ == '__main__':
